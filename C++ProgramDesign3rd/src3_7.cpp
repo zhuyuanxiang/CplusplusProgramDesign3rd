4e8581f1072f00b4d4ba168b3785245e8158fab6
zyx <ygpfr74@hotmail.com>
2021-09-19 11:27:19 +0800

    创建src3_7

---
+++ C++ProgramDesign3rd/src3_7.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void src3_7()
+{
+	// P70，嵌套调用：输入两个整数，求它们的平方和
+	cout << "--->" << "代码3-7：" << "<---" << endl;
+	int a, b;
+	int fun1(int x, int y);
+	cout << "输入 a 和 b：";
+	cin >> a >> b;
+	cout << "a,b 的平方和：" << fun1(a, b) << endl;
+}
+int fun1(int x, int y) {
+	int fun2(int m);
+	return (fun2(x) + fun2(y));
+}
+int fun2(int m) {
+	return (m * m);
+}
