29a02f27f11450c802635f7b169251cfd4bf582f
zyx <ygpfr74@hotmail.com>
2021-09-21 10:18:06 +0800

    create chap03

--- C++ProgramDesign3rd/C++ProgramDesign3rd.cpp
+++ C++ProgramDesign3rd/C++ProgramDesign3rd.cpp
@@ -2,30 +2,16 @@
 //
 
 #include <iostream>
-#include "include\chap3.h"
+#include "C++ProgramDesign3rd.h"
 
 using namespace std;
 
 int main()
 {
-	//src3_1(); cout << endl;
-	//src3_2(); cout << endl;
-	//src3_3(); cout << endl;
-	//src3_4(); cout << endl;
-	//src3_5(); cout << endl;
-	//src3_6(); cout << endl;
-	//src3_7(); cout << endl;
-	//src3_8(); cout << endl;
-	//src3_9(); cout << endl;
-	//src3_10(); cout << endl;
-	//src03_11(); cout << endl;
-	//src03_12(); cout << endl;
-	//src03_13(); cout << endl;
-	//src03_14(); cout << endl;
-	//src03_15(); cout << endl;
-	//src03_16(); cout << endl;
-	src03_17(); cout << endl;
+	chap03();
 }
+
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
