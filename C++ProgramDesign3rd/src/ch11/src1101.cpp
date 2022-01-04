b0ee7a7a1a48ad9829611aebfd1c24aeaad95e7d
zyx <zhuyuanxiang@gmail.com>
2022-01-04 11:44:39 +0800

    Create ch11, src1101

---
+++ C++ProgramDesign3rd/src/ch11/src1101.cpp
@@ -0,0 +1,12 @@
+﻿#include<iostream>
+
+using namespace std;
+
+void src1101() {
+	double values[] = { 1.23, 35.36, 653.7, 4358.24 };
+	for (int i = 0; i < 4; i++)
+	{
+		cout.width(10);
+		cout << values[i] << '\n';
+	}
+}
