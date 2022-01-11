b60b265d4f2233f7dab59e555af571cfed0c48c6
zyx <zhuyuanxiang@gmail.com>
2022-01-11 17:13:10 +0800

    Create ch11/sec1203

--- C++ProgramDesign3rd/include/ch11.h
+++ C++ProgramDesign3rd/include/ch11.h
@@ -4,3 +4,4 @@ void src1101();
 void src1102();
 void src1103();
 void src1104();
+void src1105();
---
+++ C++ProgramDesign3rd/src/ch11/src1105.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+struct Date
+{
+	int mo, da, yr;
+};
+
+void src1105() {
+	// P378，向文件输出
+	cout << "--->" << "代码11-05（向文件输出）" << "<---" << endl;
+
+	Date dt = { 6, 10, 92 };
+	ofstream tfile("tmp\\date.dat", ios_base::binary);
+	tfile.write((char*)&dt, sizeof(dt));
+	tfile.close();
+
+	cout << "文件 date.dat 输出完成，请核查！" << endl;
+}
